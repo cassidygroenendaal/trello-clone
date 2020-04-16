4192792daf38da2c91fd86289deb4c943d5e4a2f
Cassidy G <cassidygroenendaal@gmail.com>
2020-04-16 18:45:12 -0400

    Change old model1 example to use migrations

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    resetPasswordToken: DataTypes.STRING,
-    resetPasswordExpires: DataTypes.DATE
-  }, {});
+	const User = sequelize.define(
+		'User',
+		{
+			username             : DataTypes.STRING,
+			email                : DataTypes.STRING,
+			password             : DataTypes.STRING,
+			resetPasswordToken   : DataTypes.STRING,
+			resetPasswordExpires : DataTypes.DATE
+		},
+		{}
+	);
 
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  
-  return User;
+	User.associate = function(models) {
+		// associations can be defined here
+	};
+
+	return User;
 };
