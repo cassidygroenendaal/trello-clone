29a8bf384aa6d27def82cabd1f3c889a0c377005
Cassidy G <cassidygroenendaal@gmail.com>
2020-04-21 14:53:01 -0400

    Add support for label and input classes

--- client/src/components/InputGroup/index.jsx
+++ client/src/components/InputGroup/index.jsx
@@ -8,20 +8,23 @@ import React from 'react';
 
 // ----------------- InputGroup ------------------
 
-const InputGroup = (props) => {
-  return (
-    <div>
-      <label htmlFor={props.name}>{props.label}</label>
-      <input
-        type={props.type}
-        id={props.name}
-        name={props.name}
-        value={props.value}
-        placeholder={props.placeholder}
-        onChange={props.onChange}
-      />
-    </div>
-  );
+const InputGroup = props => {
+	return (
+		<div>
+			<label className={props.labelClass} htmlFor={props.name}>
+				{props.label}
+			</label>
+			<input
+				className={props.inputClass}
+				type={props.type}
+				id={props.name}
+				name={props.name}
+				value={props.value}
+				placeholder={props.placeholder}
+				onChange={props.onChange}
+			/>
+		</div>
+	);
 };
 
 export default InputGroup;
