2129e152d5a3e49f893626e00c011ebc0320bfaa
Cassidy G <cassidygroenendaal@gmail.com>
2020-04-22 10:55:39 -0400

    Remove unused import

--- client/src/pages/Reset/index.jsx
+++ client/src/pages/Reset/index.jsx
@@ -1,7 +1,6 @@
 // ----------------- Dependencies ------------------
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 // ----------------- Other Dependencies ------------------
 
