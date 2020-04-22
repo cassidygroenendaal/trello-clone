7051f04022434d56c27e9e6a26df499b5c17d443
Cassidy G <cassidygroenendaal@gmail.com>
2020-04-22 13:50:35 -0400

    Style empty list

--- client/src/components/AssignedCards/index.jsx
+++ client/src/components/AssignedCards/index.jsx
@@ -1,15 +1,44 @@
 // ----------------- Dependencies ------------------
 
-import React from 'react';
+import React, { useState } from 'react';
 
 // ----------------- Stylesheet ------------------
 
-// import './style.css';
+import styles from './style.module.css';
+
+// ----------------- Components ------------------
+
+import Button from '../Button';
 
 // ----------------- Button ------------------
 
-const AssignedCards = (props) => {
-  return <div>I'm the Assigned Cards component</div>;
+const AssignedCards = props => {
+	const [ sortedBy, setSortedBy ] = useState('board'),
+		[ filteredBy, setFilteredBy ] = useState('all boards');
+
+	return (
+		<div className={styles.container}>
+			<ul className={styles.list}>
+				<li className={styles.item}>
+					<button className={styles.btn}>
+						Sorting by <span className={styles.bold}>{sortedBy}</span>
+					</button>
+				</li>
+				<li className={styles.item}>
+					<button className={styles.btn}>
+						Showing cards from{' '}
+						<span className={styles.bold}>{filteredBy}</span>
+					</button>
+				</li>
+			</ul>
+			<div className={styles.cards}>
+				<p>
+					No visible cards. You must be added to a card for it to
+					appear here.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default AssignedCards;
