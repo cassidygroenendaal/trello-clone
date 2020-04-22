70549e4cc05079f166efd6ea9dfc22c7baaec2cc
Cassidy G <cassidygroenendaal@gmail.com>
2020-04-22 16:10:56 -0400

    Add styles for textarea

--- client/src/components/TextareaGroup/index.jsx
+++ client/src/components/TextareaGroup/index.jsx
@@ -4,18 +4,21 @@ import React from 'react';
 
 // ----------------- Stylesheet ------------------
 
-// import './style.css';
+import styles from './style.module.css';
 
 // ----------------- InputGroup ------------------
 
 const TextareaGroup = props => {
 	return (
 		<div>
-			<label className={props.labelClass} htmlFor={props.name}>
+			<label
+				className={props.labelClass || styles.hidden}
+				htmlFor={props.name}
+			>
 				{props.label}
 			</label>
 			<textarea
-				className={props.inputClass}
+				className={props.inputClass || styles.textarea}
 				id={props.name}
 				name={props.name}
 				value={props.value}
